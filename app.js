d4e62ff16f26e7132d985f2e74e210686b43d513
Gabriela Justice <gabynicole98@gmail.com>
2017-06-08 08:24:02 -0400

    got all buttons, promote, delete, up, and down, working.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const app = {
   flicks: [],
+  flickLi: [],
 
   init(formSelector){
     this.max = 0
@@ -12,8 +13,8 @@ const app = {
       id:this.max + 1,
       name:f.name.value,
     }
-    this.flicks.push(flick)
     ++this.max
+    this.flicks.push(flick)
     const listItem = this.renderListItem(flick)
     this.renderList(listItem)
     f.name.value = ' '
@@ -33,44 +34,78 @@ const app = {
     down.innerHTML = '<img src="down.png" alt="down" />'
     down.setAttribute('class', 'down')
     item.innerHTML = flick.name
+    item.setAttribute('class', `${this.flicks.indexOf(flick)}`)
     item.appendChild(del)
     item.appendChild(down)
     item.appendChild(up)
     item.appendChild(promote)
-    return item
-  },
-  renderList(item){
-    const list = document.querySelector('#flick-list')
-    list.insertBefore(item, list.childNodes[0])
+    this.flickLi.push(item)
     const buttons = item.childNodes
     this.addListeners(buttons)
+    return item
+  },
+  renderList(){
+    const list = document.querySelector('#flick-list')
+    for(var i=this.flickLi.length-1; i>=0; i--){
+      this.flickLi[i].setAttribute('class', `${i}`)
+      list.appendChild(this.flickLi[i])
+    }
   },
 
   addListeners(buttons){
      //button 0 is just the movie name
-     buttons[1].addEventListener('click', this.delItem) //deleteButton
-  //   buttons[2].addEventListener('click', moveItemDown) //downButton
-  //   buttons[3].addEventListener('click', moveItemUp) //upButton
-     buttons[4].addEventListener('click', this.promoteItem) //promoteButton
+    buttons[1].addEventListener('click', this.delItem.bind(this)) //deleteButton
+    buttons[2].addEventListener('click', this.moveItemDown.bind(this)) //downButton
+    buttons[3].addEventListener('click', this.moveItemUp.bind(this)) //upButton
+    buttons[4].addEventListener('click', this.promoteItem) //promoteButton
   },
 
   delItem(ev){
+    const list = document.querySelector('#flick-list')
     const button = ev.target.parentElement
     const li = button.parentElement
-    li.outerHTML = ''
+    list.removeChild(li)
+    let index = li.getAttribute('class')
+    index = Number(index)
+    this.flicks.splice(index, 1)
+    this.flickLi.splice(index, 1)
+    this.renderList()
   },
-  //
-  // moveItemDown(ev){
-  //
-  // },
-  //
-  // moveItemUp(ev){
-  //
-  // },
+
+  moveItemDown(ev){
+    const button = ev.target.parentElement
+    const li = button.parentElement
+    let index = this.flickLi.indexOf(li)
+    index = Number(index)
+    const swapIndex = index - 1
+    if(swapIndex >= 0){
+      const swapElem = this.flickLi[index]
+      this.flickLi[index] = this.flickLi[swapIndex]
+      this.flickLi[swapIndex] = swapElem
+      this.flicks[index] = this.flicks[swapIndex]
+      this.flicks[swapIndex] = swapElem
+      this.renderList()
+    }
+  },
+
+  moveItemUp(ev){
+    const button = ev.target.parentElement
+    const li = button.parentElement
+    let index = this.flickLi.indexOf(li)
+    index = Number(index)
+    const swapIndex = index + 1
+    if(swapIndex < this.flickLi.length){
+      const swapElem = this.flickLi[index]
+      this.flickLi[index] = this.flickLi[swapIndex]
+      this.flickLi[swapIndex] = swapElem
+      this.flicks[index] = this.flicks[swapIndex]
+      this.flicks[swapIndex] = swapElem
+      this.renderList()
+    }
+  },
 
   promoteItem(ev){
     let button = ev.target
-    console.log(button)
     if(button.getAttribute('src') == 'star1.png'){
       button.setAttribute('src', 'star.png')
       button.style.backgroundColor = 'white'
